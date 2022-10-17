9cb63c824e461503767480737342f4a8f24909a9
MANRED12 <roral@hotmail.com>
2022-10-17 11:51:30 -0500

    Intento solución Whitelabel Error Page

--- src/main/java/com/team7/computercenter/ComputerCenterApplication.java
+++ src/main/java/com/team7/computercenter/ComputerCenterApplication.java
@@ -2,8 +2,11 @@ package com.team7.computercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+
 public class ComputerCenterApplication {
 
     public static void main(String[] args) {
